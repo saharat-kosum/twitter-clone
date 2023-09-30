8149e71f8f37a6b996fd87ac0743858627647738
phai <pekopon_@live.fr>
2023-09-30 21:52:17 +0700

    edit navbar add loading component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,109 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div
+    class="flex justify-center bg-black text-white w-screen lg:h-screen min-h-screen"
+  >
+    <nav v-if="showNavbar" class="flex flex-col justify-between py-2">
+      <div>
+        <router-link to="/home">
+          <div class="hover:bg-white/[0.13] w-fit p-3 rounded-full">
+            <img class="w-7" src="../src/assets/logo-white.png" />
+          </div>
+        </router-link>
+        <router-link to="/home">
+          <div class="hover:bg-white/[0.13] w-fit p-3 rounded-full text-xl">
+            <i class="bi bi-house"></i>
+            <span class="mx-4">Home</span>
+          </div>
+        </router-link>
+        <router-link to="#">
+          <div class="hover:bg-white/[0.13] w-fit p-3 rounded-full text-xl">
+            <i class="bi bi-search"></i>
+            <span class="mx-4">Explore</span>
+          </div>
+        </router-link>
+        <router-link to="#">
+          <div class="hover:bg-white/[0.13] w-fit p-3 rounded-full text-xl">
+            <i class="bi bi-bell"></i>
+            <span class="mx-4">Notifications</span>
+          </div>
+        </router-link>
+        <router-link to="#">
+          <div class="hover:bg-white/[0.13] w-fit p-3 rounded-full text-xl">
+            <i class="bi bi-envelope"></i>
+            <span class="mx-4">Messages</span>
+          </div>
+        </router-link>
+        <router-link to="#">
+          <div class="hover:bg-white/[0.13] w-fit p-3 rounded-full text-xl">
+            <i class="bi bi-filter-square"></i>
+            <span class="mx-4">Lists</span>
+          </div>
+        </router-link>
+        <router-link to="#">
+          <div class="hover:bg-white/[0.13] w-fit p-3 rounded-full text-xl">
+            <i class="bi bi-bookmark"></i>
+            <span class="mx-4">Bookmarks</span>
+          </div>
+        </router-link>
+        <router-link to="#">
+          <div class="hover:bg-white/[0.13] w-fit p-3 rounded-full text-xl">
+            <i class="bi bi-people"></i>
+            <span class="mx-4">Communities</span>
+          </div>
+        </router-link>
+        <router-link to="#">
+          <div class="hover:bg-white/[0.13] w-fit p-3 rounded-full text-xl">
+            <i class="bi bi-twitter-x"></i>
+            <span class="mx-4">Premium</span>
+          </div>
+        </router-link>
+        <router-link to="/profile/:id">
+          <div class="hover:bg-white/[0.13] w-fit p-3 rounded-full text-xl">
+            <i class="bi bi-person"></i>
+            <span class="mx-4">Profile</span>
+          </div>
+        </router-link>
+        <router-link to="#">
+          <div class="hover:bg-white/[0.13] w-fit p-3 rounded-full text-xl">
+            <i class="bi bi-three-dots"></i>
+            <span class="mx-4">More</span>
+          </div>
+        </router-link>
+      </div>
+      <div
+        class="flex justify-between hover:bg-white/[0.13] hover:cursor-pointer p-3 rounded-full text-xl"
+      >
+        <div>Profile</div>
+        <i class="bi bi-three-dots"></i>
+      </div>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import "flowbite/dist/flowbite.min.js";
+import { defineComponent } from "vue";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-nav {
-  padding: 30px;
-}
+export default defineComponent({
+  name: "App",
+  computed: {
+    showNavbar() {
+      return this.$route.path !== "/";
+    },
+  },
+});
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Rubik", sans-serif;
+  list-style-type: none;
+  text-decoration: none !important;
 }
 </style>
