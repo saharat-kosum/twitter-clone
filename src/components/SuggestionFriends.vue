21c5a5533e7ecd07ab58a089c66c53066de1efab
phai <pekopon_@live.fr>
2023-10-01 01:24:42 +0700

    add right bar and suggestion friends

---
+++ src/components/SuggestionFriends.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div
+      class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+    >
+      <h5
+        class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+      >
+        Noteworthy technology acquisitions 2021
+      </h5>
+      <p class="font-normal text-gray-700 dark:text-gray-400">
+        Here are the biggest enterprise technology acquisitions of 2021 so far,
+        in reverse chronological order.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SuggestionFreinds",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
