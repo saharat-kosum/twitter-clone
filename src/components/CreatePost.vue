b31fdb26283dfbcae15c2773459d6822d3fc93c2
phai <pekopon_@live.fr>
2023-10-29 20:33:50 +0700

    add post component, add create post component

---
+++ src/components/CreatePost.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="flex py-3 px-5 border-solid border-b border-[#2F3336]">
+    <img
+      class="objectFit w-9 h-9 me-2 rounded-full mt-1"
+      alt="profile"
+      :src="profilePicture"
+    />
+    <div class="flex-1">
+      <input
+        type="text"
+        placeholder="What is happening?!"
+        class="w-full mb-3 leading-none bg-inherit border-none focus:ring-0"
+      />
+      <div class="flex items-center justify-between">
+        <div class="flex gap-1 text-[#1A8CD8]">
+          <div
+            class="p-2 rounded-full hover:bg-[#1A8CD8]/[0.13] hover:cursor-pointer"
+          >
+            <i class="bi bi-image"></i>
+          </div>
+          <div
+            class="p-2 rounded-full hover:bg-[#1A8CD8]/[0.13] hover:cursor-pointer"
+          >
+            <i class="bi bi-filetype-gif"></i>
+          </div>
+          <div
+            class="p-2 rounded-full hover:bg-[#1A8CD8]/[0.13] hover:cursor-pointer"
+          >
+            <i class="bi bi-list-ul"></i>
+          </div>
+          <div
+            class="p-2 rounded-full hover:bg-[#1A8CD8]/[0.13] hover:cursor-pointer"
+          >
+            <i class="bi bi-emoji-smile"></i>
+          </div>
+          <div
+            class="p-2 rounded-full hover:bg-[#1A8CD8]/[0.13] hover:cursor-pointer"
+          >
+            <i class="bi bi-calendar-week"></i>
+          </div>
+          <div
+            class="p-2 rounded-full hover:bg-[#1A8CD8]/[0.13] hover:cursor-pointer"
+          >
+            <i class="bi bi-geo-alt"></i>
+          </div>
+        </div>
+        <button
+          type="button"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Post
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref<number>(0);
+const profilePicture = process.env.VUE_APP_PROFILE_IMG;
+</script>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "CreatePost",
+});
+</script>
+
+<style scoped></style>
---
+++ src/components/Post.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="flex py-3 px-5 border-solid border-b border-[#2F3336]">
+    <img
+      class="objectFit w-9 h-9 me-2 rounded-full mt-1"
+      alt="profile"
+      :src="profilePicture"
+    />
+    <div class="flex-1">
+      <div class="flex mx-2 items-center justify-between">
+        <div class="flex gap-2">
+          <div class="font-semibold">name</div>
+          <div class="text-[#71767C]">time</div>
+        </div>
+        <div
+          class="rounded-full text-[#71767C] hover:cursor-pointer hover:text-[#1A8CD8]"
+        >
+          <i class="bi bi-three-dots"></i>
+        </div>
+      </div>
+      <div class="ms-2">capture</div>
+      <!-- <img /> -->
+      <div class="flex items-center">
+        <div class="flex w-full text-[#71767C] justify-between">
+          <div
+            class="p-2 rounded-full hover:bg-[#1D9BF0]/[0.13] hover:cursor-pointer hover:text-[#1D9BF0]"
+          >
+            <i class="bi bi-chat"></i>
+            12
+          </div>
+          <div
+            class="p-2 rounded-full hover:bg-[#00BA7C]/[0.13] hover:cursor-pointer hover:text-[#00BA7C]"
+          >
+            <i class="bi bi-arrow-down-up"></i>
+          </div>
+          <div
+            class="p-2 rounded-full hover:bg-[#DE1673]/[0.13] hover:cursor-pointer hover:text-[#DE1673]"
+          >
+            <i class="bi bi-heart"></i>
+            12
+          </div>
+          <div
+            class="p-2 rounded-full hover:bg-[#1D9BF0]/[0.13] hover:cursor-pointer hover:text-[#1D9BF0]"
+          >
+            <i class="bi bi-bar-chart-line-fill"></i>
+          </div>
+          <div
+            class="p-2 rounded-full hover:bg-[#1D9BF0]/[0.13] hover:cursor-pointer hover:text-[#1D9BF0]"
+          >
+            <i class="bi bi-upload"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PostComponent",
+  setup() {
+    const profilePicture = process.env.VUE_APP_PROFILE_IMG;
+    return { profilePicture };
+  },
+});
+</script>
+
+<style scoped></style>
