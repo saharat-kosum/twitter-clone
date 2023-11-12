efac066a2aa011f62e02bd1ec7ff534c4ff04b69
phai <pekopon_@live.fr>
2023-11-12 17:58:53 +0700

    add new feature

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex gap-7">
+  <div v-if="isLogin" class="flex lg:gap-7 md:mb-0 mb-[64px]">
+    <LoadingComponent :isOpen="isLoadingOpen" />
     <div
-      style="max-width: 600px"
-      class="border-solid border-x border-[#2F3336] w-[600px]"
+      class="border-solid border-x border-[#2F3336] md:w-[600px] md:max-w-[600px]"
     >
       <div class="border-b border-gray-200 dark:border-gray-700">
         <ul class="flex -mb-px text-sm font-medium text-center text-[#71767C]">
@@ -20,9 +20,10 @@
           </li>
           <li class="w-1/2">
             <button
-              class="inline-block hover:bg-white/[0.1] w-full p-4 border-b-2 border-transparent rounded-t-lg"
+              class="inline-block hover:bg-white/[0.1] w-full p-4 border-b-2 rounded-t-lg"
               :class="{
                 'text-white font-semibold border-[#1A8CD8]': activeTab === 2,
+                'border-transparent': activeTab === 1,
               }"
               @click="activateTab(2)"
             >
@@ -33,34 +34,247 @@
       </div>
       <div>
         <div v-show="activeTab === 1">
-          <CreatePost />
-          <PostComponent />
+          <CreatePost
+            :userData="user"
+            :loading="setLoading"
+            :posts="posts"
+            :setPosts="setPost"
+            :sort="sortPost"
+            :token="getUserToken()"
+          />
+          <div v-if="user">
+            <PostComponent
+              v-for="post in posts"
+              :key="post._id"
+              :post="post"
+              :deletePost="deletePost"
+              :likePost="likePost"
+              :user="user"
+            />
+          </div>
+          <div
+            class="w-full text-center mt-36 text-[#71767C]"
+            v-if="posts?.length == 0"
+          >
+            No post yet
+          </div>
         </div>
         <div v-show="activeTab === 2">
-          <CreatePost />
-          <PostComponent />
+          <CreatePost
+            :userData="user"
+            :loading="setLoading"
+            :posts="posts"
+            :setPosts="setPost"
+            :sort="sortPost"
+            :token="getUserToken()"
+          />
+          <div v-if="user">
+            <PostComponent
+              v-for="post in posts"
+              :key="post._id"
+              :post="post"
+              :deletePost="deletePost"
+              :likePost="likePost"
+              :user="user"
+            />
+          </div>
+          <div
+            class="w-full text-center mt-36 text-[#71767C]"
+            v-if="posts?.length == 0"
+          >
+            No post yet
+          </div>
         </div>
       </div>
     </div>
-    <RightBarComponent />
+    <RightBarComponent class="hidden lg:block" :loading="setLoading" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import RightBarComponent from "../components/RightBar.vue";
 import CreatePost from "../components/CreatePost.vue";
 import PostComponent from "../components/Post.vue";
+import LoadingComponent from "../components/Loading.vue";
+import { PostType, UserType } from "../type";
+import axios from "axios";
 
 export default defineComponent({
   name: "HomePage",
-  components: { RightBarComponent, CreatePost, PostComponent },
+  components: {
+    RightBarComponent,
+    CreatePost,
+    PostComponent,
+    LoadingComponent,
+  },
   setup() {
+    const isLoadingOpen = ref(false);
     const activeTab = ref(1);
-    function activateTab(tabNumber: number) {
+    const isLogin = ref(false);
+    const posts = ref<PostType[] | undefined>(undefined);
+    const user = ref<UserType | undefined>(undefined);
+    const prefixURL = process.env.VUE_APP_PREFIX_URL;
+
+    const activateTab = (tabNumber: number) => {
       activeTab.value = tabNumber;
-    }
-    return { activeTab, activateTab };
+    };
+
+    const setLoading = (newValue: boolean) => {
+      isLoadingOpen.value = newValue;
+    };
+
+    const setPost = (newValue: PostType[] | undefined) => {
+      posts.value = newValue;
+    };
+
+    onMounted(() => {
+      const token = getUserToken();
+      if (token) {
+        isLogin.value = true;
+        getFeedPosts(token);
+        getUserDetail(token);
+      }
+    });
+
+    const getUserToken = () => {
+      const token = sessionStorage.getItem("userToken");
+      if (token && token.length > 0) {
+        return token;
+      } else {
+        window.location.replace("/");
+      }
+    };
+
+    const getFeedPosts = async (token: string) => {
+      isLoadingOpen.value = true;
+      try {
+        const response = await axios.get(`${prefixURL}/posts`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = response.data;
+        const sortData = sortPost(data);
+        posts.value = sortData;
+      } catch (err) {
+        console.error(err);
+      } finally {
+        isLoadingOpen.value = false;
+      }
+    };
+
+    const getUserDetail = async (token: string) => {
+      isLoadingOpen.value = true;
+      try {
+        const response = await axios.get(`${prefixURL}/users`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = response.data;
+        user.value = data;
+      } catch (err) {
+        console.error(err);
+      } finally {
+        isLoadingOpen.value = false;
+      }
+    };
+
+    const deletePost = async (id: string) => {
+      const result = window.confirm(
+        "Are you sure you want to delete this post?"
+      );
+      if (result) {
+        const token = getUserToken();
+        isLoadingOpen.value = true;
+        try {
+          const response = await axios.delete(
+            `${prefixURL}/posts/delete/${id}`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          if (response.status === 200) {
+            const filter = posts.value?.filter((post) => post._id !== id);
+            if (filter) {
+              const sortData = sortPost(filter);
+              posts.value = sortData;
+            }
+          } else if (response.status === 404) {
+            alert("Post not found");
+          } else {
+            alert("Delete post failed");
+          }
+        } catch (error) {
+          console.error(error);
+        } finally {
+          isLoadingOpen.value = false;
+        }
+      }
+    };
+
+    const likePost = async (id: string) => {
+      const token = getUserToken();
+      isLoadingOpen.value = true;
+      try {
+        const response = await axios.put(
+          `${prefixURL}/posts/${id}/like`,
+          { userId: user.value?._id },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const data: PostType = response.data;
+        const updatedPosts = posts.value?.map((post) => {
+          if (post._id === data._id) {
+            return data;
+          }
+          return post;
+        });
+        posts.value = updatedPosts;
+      } catch (error) {
+        console.error(error);
+      } finally {
+        isLoadingOpen.value = false;
+      }
+    };
+
+    const sortPost = (posts: PostType[]): PostType[] => {
+      return posts.slice().sort((a, b) => {
+        if (a.createdAt === undefined && b.createdAt === undefined) {
+          return 0;
+        }
+        if (a.createdAt === undefined) {
+          return 1;
+        }
+        if (b.createdAt === undefined) {
+          return -1;
+        }
+        return (
+          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        );
+      });
+    };
+
+    return {
+      activeTab,
+      activateTab,
+      isLoadingOpen,
+      isLogin,
+      posts,
+      user,
+      setLoading,
+      sortPost,
+      setPost,
+      getUserToken,
+      deletePost,
+      likePost,
+    };
   },
 });
 </script>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="flex gap-7">
+  <div v-if="isLogin" class="flex lg:gap-7 md:mb-0 mb-[64px]">
     <LoadingComponent :isOpen="isLoadingOpen" />
     <div
-      style="max-width: 600px"
-      class="border-solid border-x border-[#2F3336] w-[600px]"
+      class="border-solid border-x border-[#2F3336] md:w-[600px] md:max-w-[600px] w-screen"
     >
       <div class="flex gap-3 items-center ms-3 mt-1">
         <router-link
@@ -12,9 +11,14 @@
         >
           <i class="bi bi-arrow-left"></i>
         </router-link>
-        <div>Username</div>
+        <div>Back</div>
       </div>
-      <ProfileHeader />
+      <ProfileHeader
+        v-if="friendData"
+        :friendData="friendData"
+        :isSameUser="isSameUser"
+        :isFollow="isFollow"
+      />
       <div class="border-b border-gray-200 dark:border-gray-700">
         <ul class="flex -mb-px text-sm font-medium text-center text-[#71767C]">
           <li class="w-1/2">
@@ -40,7 +44,7 @@
               Replies
             </button>
           </li>
-          <li class="w-1/2">
+          <li class="w-1/2 sm:list-item hidden">
             <button
               class="inline-block hover:bg-white/[0.1] w-full p-4 border-b-2"
               :class="{
@@ -51,7 +55,7 @@
               Highlights
             </button>
           </li>
-          <li class="w-1/2">
+          <li class="w-1/2 sm:list-item hidden">
             <button
               class="inline-block hover:bg-white/[0.1] w-full p-4 border-b-2"
               :class="{
@@ -77,20 +81,38 @@
       </div>
       <div>
         <div v-show="activeTab === 1">
-          <PostComponent />
+          <div v-if="user">
+            <PostComponent
+              v-for="post in posts"
+              :key="post._id"
+              :post="post"
+              :deletePost="deletePost"
+              :likePost="likePost"
+              :user="user"
+            />
+          </div>
+          <div
+            class="w-full text-center mt-36 text-[#71767C]"
+            v-if="posts?.length == 0"
+          >
+            No post yet
+          </div>
         </div>
       </div>
     </div>
-    <RightBarComponent />
+    <RightBarComponent class="hidden lg:block" :loading="setLoading" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { computed, defineComponent, onMounted, ref, watch } from "vue";
 import RightBarComponent from "../components/RightBar.vue";
 import PostComponent from "../components/Post.vue";
 import ProfileHeader from "../components/ProfileHeader.vue";
 import LoadingComponent from "../components/Loading.vue";
+import { PostType, UserType } from "../type";
+import axios from "axios";
+import { useRoute } from "vue-router";
 
 export default defineComponent({
   name: "ProfilePage",
@@ -101,12 +123,213 @@ export default defineComponent({
     LoadingComponent,
   },
   setup() {
+    const route = useRoute();
     const isLoadingOpen = ref(false);
+    const prefixURL = process.env.VUE_APP_PREFIX_URL;
+    const isLogin = ref(false);
+    const isSameUser = computed(() => friendId.value === user.value?._id);
     const activeTab = ref(1);
+    const posts = ref<PostType[] | undefined>(undefined);
+    const user = ref<UserType | undefined>(undefined);
+    const friendData = ref<UserType | undefined>(undefined);
+    const friendId = computed(() => route.params.id as string);
+    const isFollow = computed(() => {
+      if (user.value && friendData.value?._id && user.value.friends) {
+        return user.value.friends.includes(friendData.value._id);
+      }
+      return false;
+    });
     function activateTab(tabNumber: number) {
       activeTab.value = tabNumber;
     }
-    return { activeTab, activateTab, isLoadingOpen };
+
+    onMounted(() => {
+      const token = getUserToken();
+      if (token) {
+        isLogin.value = true;
+        getPosts(token);
+        getFriendDetail(token);
+        getUserDetail(token);
+      }
+    });
+
+    watch(friendId, (newFriendId: string) => {
+      if (newFriendId) {
+        const token = getUserToken();
+        if (token) {
+          isLogin.value = true;
+          getPosts(token);
+          getFriendDetail(token);
+          getUserDetail(token);
+        }
+      }
+    });
+
+    const getUserToken = () => {
+      const token = sessionStorage.getItem("userToken");
+      if (token && token.length > 0) {
+        return token;
+      } else {
+        window.location.replace("/");
+      }
+    };
+
+    const getPosts = async (token: string) => {
+      isLoadingOpen.value = true;
+      try {
+        const response = await axios.get(
+          `${prefixURL}/posts/${friendId.value}/posts`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const data = response.data;
+        const sortData = sortPost(data);
+        posts.value = sortData;
+      } catch (err) {
+        console.error(err);
+      } finally {
+        isLoadingOpen.value = false;
+      }
+    };
+
+    const getFriendDetail = async (token: string) => {
+      isLoadingOpen.value = true;
+      try {
+        const response = await axios.get(
+          `${prefixURL}/users/friend/${friendId.value}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const data = response.data;
+        friendData.value = data;
+      } catch (err) {
+        console.error(err);
+      } finally {
+        isLoadingOpen.value = false;
+      }
+    };
+
+    const getUserDetail = async (token: string) => {
+      isLoadingOpen.value = true;
+      try {
+        const response = await axios.get(`${prefixURL}/users`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = response.data;
+        user.value = data;
+      } catch (err) {
+        console.error(err);
+      } finally {
+        isLoadingOpen.value = false;
+      }
+    };
+
+    const sortPost = (posts: PostType[]): PostType[] => {
+      return posts.slice().sort((a, b) => {
+        if (a.createdAt === undefined && b.createdAt === undefined) {
+          return 0;
+        }
+        if (a.createdAt === undefined) {
+          return 1;
+        }
+        if (b.createdAt === undefined) {
+          return -1;
+        }
+        return (
+          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        );
+      });
+    };
+
+    const likePost = async (id: string) => {
+      const token = getUserToken();
+      isLoadingOpen.value = true;
+      try {
+        const response = await axios.put(
+          `${prefixURL}/posts/${id}/like`,
+          { userId: user.value?._id },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const data: PostType = response.data;
+        const updatedPosts = posts.value?.map((post) => {
+          if (post._id === data._id) {
+            return data;
+          }
+          return post;
+        });
+        posts.value = updatedPosts;
+      } catch (error) {
+        console.error(error);
+      } finally {
+        isLoadingOpen.value = false;
+      }
+    };
+
+    const setLoading = (newValue: boolean) => {
+      isLoadingOpen.value = newValue;
+    };
+
+    const deletePost = async (id: string) => {
+      const result = window.confirm(
+        "Are you sure you want to delete this post?"
+      );
+      if (result) {
+        const token = getUserToken();
+        isLoadingOpen.value = true;
+        try {
+          const response = await axios.delete(
+            `${prefixURL}/posts/delete/${id}`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          if (response.status === 200) {
+            const filter = posts.value?.filter((post) => post._id !== id);
+            if (filter) {
+              const sortData = sortPost(filter);
+              posts.value = sortData;
+            }
+          } else if (response.status === 404) {
+            alert("Post not found");
+          } else {
+            alert("Delete post failed");
+          }
+        } catch (error) {
+          console.error(error);
+        } finally {
+          isLoadingOpen.value = false;
+        }
+      }
+    };
+
+    return {
+      activeTab,
+      activateTab,
+      isLoadingOpen,
+      isLogin,
+      posts,
+      setLoading,
+      friendData,
+      isSameUser,
+      isFollow,
+      deletePost,
+      likePost,
+      user,
+    };
   },
 });
 </script>
