6a6f63c7871ac92026e30771cd9eb7c4efa1ed8c
phai <pekopon_@live.fr>
2023-11-28 23:48:33 +0700

    add onclick function in follow and unfollow button

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -41,12 +41,14 @@
               <li
                 v-if="!isFriend"
                 class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                @click="addFriend(post.userId)"
               >
                 Follow
               </li>
               <li
                 v-else
                 class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                @click="removeFriend(post.userId)"
               >
                 Unfollow
               </li>
